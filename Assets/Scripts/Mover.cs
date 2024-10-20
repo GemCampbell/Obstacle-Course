f68e20c2f6b6b6935777d9480792c829d964f9a6
Gemma Campbell <gem.cam89@outlook.com>
2024-10-20 21:08:49 +0100

    Added: OnCollisionEnter - GetComponent - Score and ObjectHit scripts

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,14 +8,30 @@ public class Mover : MonoBehaviour
 
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     void Update()
     {
+        MovePlayer();
+    }
+
+
+
+    void PrintInstructions()
+    {
+        print("Welcome to the game!");
+        print("Move the player with the arrow keys");
+        print("Don't bump into the walls!");
+    }
+
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float zValue = Input.GetAxis("Vertical") *moveSpeed * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         transform.Translate(xValue, 0, zValue);
     }
+
 }
---
+++ Assets/Scripts/ObjectHit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        GetComponent<MeshRenderer>().material.color = Color.green;
+
+    }
+
+}
